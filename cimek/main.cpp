5976fda1d9073124a5e784f1d02120bfd24eb68d
Daniel Spindelbauer <spindel.daniel97@gmail.com>
2016-02-15 17:32:32 +0100

    Initial Commit

---
+++ cimek/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cimek
+//
+//  Created by sdaniel on 15/02/16.
+//  Copyright © 2016 Daniel Spindelbauer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
