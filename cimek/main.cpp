84a5725ae7bd97f8e319fe2d80f9bb193d7de1f4
Daniel Spindelbauer <spindel.daniel97@gmail.com>
2016-02-17 17:59:48 +0100

    Added task description

--- cimek/main.cpp
+++ cimek/main.cpp
@@ -6,6 +6,19 @@
 //  Copyright © 2016 Daniel Spindelbauer. All rights reserved.
 //
 
+
+// 1. Olvassa be az ip.txt állományban talált adatokat, s annak felhasználásával oldja meg a következő feladatokat!
+// 2. Határozza meg és írja a képernyőre, hogy hány adatsor van az állományban!
+// 3. Írja a képernyőre az állományban található legalacsonyabb IP-címet! A megoldásában felhasználhatja, hogy a betűk ASCII-kódjai a számok ASCII-kódjai után találhatók a kódtáblában.
+// 4. Határozza meg, hogy az állományban hány darab IP-cím van az egyes fajtákból! Az eredményt jelenítse meg a képernyőn a mintának megfelelően!
+// 5. Gyűjtse ki a sok.txt állományba azokat az IP-címeket, melyek legalább 18 nullát tartalmaznak! A fájlban minden sor elején szerepeljen az eredeti állományból a cím sorszáma! Ezt kövesse egy szóközzel elválasztva a cím az ip.txt állományban szereplő alakjával!
+// 6. Kérjen be a felhasználótól egy sorszámot! Az állományban a megadott sorszámon található IP-címet rövidítse a csoportokon belüli bevezető nullák elhagyásával! Az állományban található alakot és a rövidített változatot írja a képernyőre egymás alá!
+// 7. Az előző feladatban használt IP-címet rövidítse tovább az egymást követő nullás csoportok rövidítésére vonatkozó szabályoknak megfelelően! Az eredményt jelenítse meg a képernyőn! Amennyiben nem rövidíthető, írja ki: „Nem rövidíthető tovább.”!
+
+
+
+
+
 #include <iostream>
 #include <fstream>
 #include <string>
